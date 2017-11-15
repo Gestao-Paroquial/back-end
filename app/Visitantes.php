fc1f06fe202aa79d53b5e3b4f147cf5f2a7e70e6
Leonardo Cardoso <leocardosoti@gmail.com>
2017-11-15 21:06:41 -0200

    Relacionamento Visitante

--- app/Visitantes.php
+++ app/Visitantes.php
@@ -9,4 +9,10 @@ class Visitantes extends Model
     protected $fillable = [
         'comunidades_id','name', 'email', 'telefone'
     ];
+
+
+    public function comunidades()
+    {
+        return $this->belongsTo(Comunidades::class, "comunidades_id", "id");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,34 +12,30 @@
 */
 
 
-Route::group(['middleware' => ['web']], function(){
+Route::group(['middleware' => ['web']], function () {
     Route::get('/', function () {
-        return view('welcome'); 
+        return view('welcome');
     });
-    Route::get('foo', function(){
+    Route::get('foo', function () {
         return "método GET";
     });
-    Route::get('foo', function(){
+    Route::get('foo', function () {
         return view('bar', ['foo' => '<font color = "blue">teste</font>']);
     });
-    Route::group(['prefix' => 'user'], function(){
-        Route::get('/', ['uses' => 'UsersController@index']);        
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/', ['uses' => 'UsersController@index']);
         Route::get('add', ['uses' => 'UsersController@create']);
-        Route::post('add', ['uses' => 'UsersController@post']); 
-    });     
+        Route::post('add', ['uses' => 'UsersController@post']);
+    });
     Route::get('api/usuarios', ['uses' => 'UsersController@usuario']);
 });
 Route::resource('note', 'NoteController');
 Route::resource('api/pastorais', 'PastoraisController');
 Route::resource('api/comunidades', 'ComunidadesController');
-// Route::resource('api/membros', 'MembrosController');
+Route::resource('api/visitantes', 'VisitantesController');
 Route::resource('api/membrosPastorais', 'MembrosPastoraisController');
 
 
 // Route::group(['middleware' => 'cors'], function(){
 //     Route::resource('api/comunidades', 'ComunidadesController');
 // });
-
-
-
-
