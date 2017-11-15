5d13ecc73ec6050735fbec36a7c1ad9e51cb8784
Leonardo Cardoso <leocardosoti@gmail.com>
2017-11-15 20:58:47 -0200

    Relação de membro com pastoral

--- app/Http/Controllers/MembrosPastoraisController.php
+++ app/Http/Controllers/MembrosPastoraisController.php
@@ -7,24 +7,44 @@ use App\MembrosPastorais;
 
 class MembrosPastoraisController extends Controller
 {
-    public function index(){
-        return response()->json(MembrosPastorais::all());
+    public function index()
+    {
+
+        $ep = new MembrosPastorais();
+        
+        $result = $ep->with('pastorais')        
+        ->get();
+
+        return $result; 
+
+        // return response()->json(MembrosPastorais::all());
     }
-    public function show($id){
-        $membrosPastorais = MembrosPastorais::findOrFail($id);
-        return response()->json($membrosPastorais);
+    public function show($id)
+    {
+        $ep = new MembrosPastorais();
+        
+        $result = $ep->with('pastorais')
+        ->where('id', $id)
+        ->first();
+
+        return $result; 
+        // $membrosPastorais = MembrosPastorais::findOrFail($id);
+        // return response()->json($membrosPastorais);
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $membrosPastorais = MembrosPastorais::create($request->all());
         return response()->json($membrosPastorais);
     }
-    public function update(Request $request, $id){
+    public function update(Request $request, $id)
+    {
         $membrosPastorais = MembrosPastorais::findOrFail($id);
         $membrosPastorais->fill($request->all());
         $membrosPastorais->save();
         return response()->json($membrosPastorais);
     }
-    public function destroy($id){
+    public function destroy($id)
+    {
         $membrosPastorais = Comunidades::findOrFail($id);
         $membrosPastorais->delete();
         return response()->json(['message'=>'removido com sucesso']);
--- app/MembrosPastorais.php
+++ app/MembrosPastorais.php
@@ -6,10 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class MembrosPastorais extends Model
 {
-	protected $fillable = [
-        'comunidades_id','pastorais_id'	,'nome','email',
-		'telefone','celular','endereco','nro','compl',
-		'bairro','cidade','uf','cep', 
+    protected $fillable = [
+        'pastorais_id'  ,'nome','email',
+        'telefone','celular','endereco','nro','compl',
+        'bairro','cidade','uf','cep',
     ];
 
+    public function pastorais()
+    {
+        return $this->belongsTo(Pastorais::class, "pastorais_id", "id");
+    }	
 }
