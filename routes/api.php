63b430b594532aca7b5d63a985781e4b7be69367
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-03-07 12:11:19 -0300

    create routes

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('uploadImagem', ['uses' => 'ImagesController@save']);
 Route::resource('note', 'NoteController');
 Route::resource('pastorais', 'PastoraisController');
@@ -25,3 +21,15 @@ Route::resource('visitantes', 'VisitantesController');
 Route::resource('membrosPastorais', 'MembrosPastoraisController');
 Route::resource('mensagensParoco', 'MensagensParocoController');
 Route::resource('eventosHome', 'EventosHomeController');
+Route::resource('user', 'UsersController');
+
+Route::post('register', 'AuthController@register');
+Route::post('login', 'AuthController@login');
+Route::post('recover', 'AuthController@recover');
+
+Route::group(['middleware' => ['jwt.auth']], function() {
+    Route::get('logout', 'AuthController@logout');
+    Route::get('test', function(){
+        return response()->json(['foo'=>'bar']);
+    });
+});
