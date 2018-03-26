4865667cad4cd11736b24d9a6c9b4a914af06d27
Marco Ferreira <marco.ferreira@lojasmel.com>
2018-03-25 23:19:10 -0300

    Inclusão da arquitetura base do PagSeguro e criação do Namespace

---
+++ app/Http/Controllers/GetStatusController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GetStatusController extends Controller
+{
+    /*
+    require_once("PagSeguro.class.php");
+
+	if(isset($_GET['reference'])){
+		$PagSeguro = new PagSeguro();
+		$P = $PagSeguro->getStatusByReference($_GET['reference']);
+		echo $PagSeguro->getStatusText($P->status);
+	}else{
+	    echo "Parâmetro \"reference\" não informado!";
+	}
+    */
+}
---
+++ app/Http/Controllers/NotificacaoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NotificacaoController extends Controller
+{
+  /*
+  header("access-control-allow-origin: https://pagseguro.uol.com.br");
+	require_once("PagSeguro.class.php");
+
+	if(isset($_POST['notificationType']) && $_POST['notificationType'] == 'transaction'){
+		$PagSeguro = new PagSeguro();
+		$response = $PagSeguro->executeNotification($_POST);
+		if( $response->status==3 || $response->status==4 ){
+        	//PAGAMENTO CONFIRMADO
+			//ATUALIZAR O STATUS NO BANCO DE DADOS
+			
+		}else{
+			//PAGAMENTO PENDENTE
+			echo $PagSeguro->getStatusText($PagSeguro->status);
+		}
+	}*/  
+}
