b13275ae7409c669638f773c0d67f7b98fccef15
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-03-27 14:56:45 -0300

    adicionando rota tipo membro

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::resource('dizimos', 'DizimosController');
 Route::resource('doacoes', 'DoacoesController');
 Route::resource('eventosHome', 'EventosHomeController');
 Route::resource('user', 'UsersController');
+Route::resource('tipoMembro', 'TipoMembroController');
 
 Route::get('analytics', 'AnalyticsController@getData');
 
