92d1642058eebe1de8e026b821c219f7bd020c08
Leonardo Cardoso <leocardosoTI@gmail.com>
2018-04-30 15:34:22 -0300

    removendo arquivos do site

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,7 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@postReset')->name('password.reset');
 
 Route::group(['middleware' => ['web']], function () {
-    Route::get('/', function () {
-        return File::get(public_path() . '/index.html');
-    });
     Route::get('foo', function () {
-        return "método GET";
-    });
-    Route::get('foo', function () {
-        return view('bar', ['foo' => '<font color = "blue">teste</font>']);
-    });
-    Route::group(['prefix' => 'user'], function () {
-        Route::get('/', ['uses' => 'UsersController@index']);
-        Route::get('add', ['uses' => 'UsersController@create']);
-        Route::post('add', ['uses' => 'UsersController@post']);
+        return "bar";
     });
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
