3d7b66617eba171ead500263c63ff0c7c6321afd
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-01 15:33:31 -0300

    alterando template do email

--- resources/views/email/pedidoAprovado.blade.php
+++ resources/views/email/pedidoAprovado.blade.php
@@ -1,25 +1,19 @@
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-    <meta charset="utf-8">
-</head>
-<body>
-<div>
-    Olá {{ $nome }},
-    <br>
-    <br>
-    Seu pedido foi aprovado pelo Padre!
-    <br>
-    <br>
-    Por favor, efetue o pagamento para confirmarmos o pedido.
-    <br>
+@component('mail::message')
+# Pedido Aprovado
 
-    <a href="{{$link}}">Clique aqui para efetuar o pagamento </a>
-    <br>
-    <br>
-    Obrigado,<br>
-    Paroquia São Lucas Evangelista
-</div>
+Olá {{ $nome }},
+<br>
+<br>
+Seu pedido foi aprovado pelo Padre!
+<br>
+<br>
+Por favor, efetue o pagamento para confirmarmos o pedido.
+<br>
 
-</body>
-</html>
+@component('mail::button', ['url' => $link])
+Clique aqui para efetuar o pagamento
+@endcomponent
+
+Obrigado,<br>
+Paroquia São Lucas Evangelista
+@endcomponent
