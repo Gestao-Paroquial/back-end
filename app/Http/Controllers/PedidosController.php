60edf446979e58e12610ba51e4d62f0bab7e0d71
Leonardo Cardoso <leocardosoti@gmail.com>
2018-05-01 15:59:55 -0300

    fazendo checkout do batismo

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -22,8 +22,10 @@ class PedidosController extends Controller
         $pedido = Pedido::findOrFail($id);
         $pedido->fill($request->all());
         $pedido->save();
-        if ($pedido->aprovado == 1) {
+        if ($pedido->aprovado == 1 && $pedido->casamento == 1) {
             $this->checkoutCasamento($pedido);
+        } else if ($pedido->aprovado == 1 && $pedido->batismo == 1) {
+            $this->checkoutBatismo($pedido);
         } else if ($pedido->aprovado == 2) {
             $this->enviarEmailDeReprovacao($pedido, $request->conteudoEmail);
         }
@@ -56,9 +58,48 @@ class PedidosController extends Controller
         $pedido->cpf = $request->cpf;
         $pedido->telefone = $request->telefone;
 
-
         $pedido->save();
         return response()->json(['success' => true]);
+    }
+
+    public function checkoutBatismo($pedido)
+    {
+        $data = [
+            'items' => [
+                [
+                    'id' => $pedido->id,
+                    'description' => 'Batismo',
+                    'quantity' => 1,
+                    'amount' => 50,
+                ],
+            ],
+            'sender' => [
+                'name' => $pedido->nome,
+                'documents' => [
+                    [
+                        'number' => $pedido->cpf,
+                        'type' => 'CPF',
+                    ],
+                ],
+            ],
+            'currency' => 'BRL',
+        ];
+
+        $this->checkout($data, $pedido);
+    }
+
+    public function checkout($data, $pedido)
+    {
+        $checkout = PagSeguro::checkout()->createFromArray($data);
+        $credentials = PagSeguro::credentials()->get();
+        $information = $checkout->send($credentials); // Retorna um objeto de laravel\pagseguro\Checkout\Information\Information
+        if ($information) {
+            $pedido->code = 1;
+            $pedido->data_do_checkout = $information->getDate();
+            $pedido->link = $information->getLink();
+            $pedido->save();
+            $this->enviarEmailDeAprovacao($pedido);
+        }
     }
 
     public function checkoutCasamento($pedido)
@@ -84,16 +125,8 @@ class PedidosController extends Controller
             'currency' => 'BRL',
         ];
 
-        $checkout = PagSeguro::checkout()->createFromArray($data);
-        $credentials = PagSeguro::credentials()->get();
-        $information = $checkout->send($credentials); // Retorna um objeto de laravel\pagseguro\Checkout\Information\Information
-        if ($information) {
-            $pedido->code = 1;
-            $pedido->data_do_checkout = $information->getDate();
-            $pedido->link = $information->getLink();
-            $pedido->save();
-            $this->enviarEmailDeAprovacao($pedido);
-        }
+        $this->checkout($data, $pedido);
+
     }
 
     public function enviarEmailDeAprovacao($pedido)
